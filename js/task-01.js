1047571d7f31396d00c6fa60d2076d2b7899687a
MenkivOleg <laures04@gmail.com>
2023-06-17 22:11:34 +0300

    done 02 task

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
 const categoriesList = document.querySelectorAll('#categories .item');
 
-console.log('Number of categories:', categoriesList.length);
+console.dir('Number of categories:', categoriesList.length);
 
 categoriesList.forEach(category => {
   const categoryName = category.querySelector('h2').textContent;
   const categoryElements = category.querySelectorAll('li').length;
 
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElements}`);
+  console.dir(`Category: ${categoryName}`);
+  console.dir(`Elements: ${categoryElements}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ul = document.querySelector('#ingredients');
+ingredients.forEach((ingredients) => {
+  const li = document.createElement('li')
+  li.classList.add('item');
+  li.textContent = ingredients;
+  ul.append(li);
+});
+
+// const li = document.createElement('li');
+// li.classList.add('item')
+// li.textContent = ingredients;
+// ul.append(li);
