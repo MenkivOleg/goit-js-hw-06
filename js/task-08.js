1fb05461ea019f06caa02d7ba48543e7834f0cf6
MenkivOleg <laures04@gmail.com>
2023-06-19 20:37:06 +0300

    Update 08 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function(evt) {
+  evt.preventDefault(); 
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('Всі поля повинні бути заповнені!');
+  } else {
+    const loginData = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+
+    console.log(loginData);
+    loginForm.reset();
+  }
+});
